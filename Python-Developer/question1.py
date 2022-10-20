65ac0021223619e0565906730d1a318c131828c6
Prateek Alakh <thunder@Prateeks-MacBook-Air.local>
2022-10-20 10:12:42 -0400

    Question 1 python completed

--- Python-Developer/question1.py
+++ Python-Developer/question1.py
@@ -1,9 +1,18 @@
 def select_max(array):
     # write your function here
     # do NOT use the built-in max() function
-    pass
+    if len(array)<=0:
+        return
+
+    max=array[0]
+    for i in array[1:]:
+        if max<i:
+            max=i
+    return max
 
 
 if __name__ == "__main__":
     # write your debug code here
+    print(select_max([1,2,5,0,3]))
+    print(select_max([]))
     pass
