2d75aaa784e2a26f62a90ecfcb9e539bdbcedb1d
Prateek Alakh <thunder@Prateeks-MacBook-Air.local>
2022-10-20 11:13:04 -0400

    Completed all the python dev queries

--- Python-Developer/question2.py
+++ Python-Developer/question2.py
@@ -5,9 +5,23 @@ def center_zeros(array):
     # write your function here
     # center means the floor(x / 2) where floor means rounding a float (decimal number) down to the nearest integer
     # i.e. floor(1) = 1, floor(1.5) = 1, floor(1.75) = 1, floor(2) = 2
-    pass
 
+    if 0 not in array:
+        return array
+
+    number_of_zeroes=0
+    for i in array:
+        if i == 0:
+            number_of_zeroes+=1
+            array.remove(0)
+    
+    center_of_arr=floor(len(array)/2)
+    
+    return array[:center_of_arr]+([0]*number_of_zeroes)+array[center_of_arr:]
 
 if __name__ == "__main__":
     # write your debug code here
+    print(center_zeros([1, 1, 3, 0, 6, 0]))
+    print(center_zeros([]))
+    
     pass
--- Python-Developer/question_3.py
+++ Python-Developer/question_3.py
@@ -1,8 +1,21 @@
+from math import floor
+
 def minimum_points(threshold, points):
     # write your code here
-    pass
+    if len(points)<3:
+        return len(points)
+    i=2
+    while i<len(points):
+        if i==len(points):
+            return len(points)
+        if points[i]-points[0]>=threshold:
+            return floor((i+1)/2)+1
+        i+=1
+    return len(points)
+    
 
 
 if __name__ == "__main__":
     # write your debug code here
+    print(minimum_points(2, [1, 2, 3]))
     pass
